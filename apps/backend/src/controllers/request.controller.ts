198c4bae243ba72839a2e820189c648f1895514e
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-05-30 21:41:37 +0530

    feat: add get request API

--- apps/backend/src/controllers/request.controller.ts
+++ apps/backend/src/controllers/request.controller.ts
@@ -1,11 +1,73 @@
 import { Response } from "express";
-import mongoose from "mongoose";
+import mongoose, { PipelineStage } from "mongoose";
 import { Request } from "../models/request.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { RequestExpress } from "../utils/types.js";
+
+export const getRequests = asyncHandler(
+  async (req: RequestExpress, res: Response) => {
+    const pipeline: PipelineStage[] = [];
+
+    if (req.query?.type === "received") {
+      pipeline.push({
+        $match: {
+          receiver: req.user._id,
+        },
+      });
+    } else {
+      pipeline.push({
+        $match: {
+          sender: req.user._id,
+        },
+      });
+    }
+
+    pipeline.push(
+      {
+        $lookup: {
+          from: "users",
+          localField: req.query?.type == "received" ? "sender" : "receiver",
+          foreignField: "_id",
+          as: "user_details",
+        },
+      },
+      {
+        $unwind: "$user_details",
+      },
+      {
+        $project: {
+          user_details: 1,
+        },
+      },
+      {
+        $addFields: {
+          requestId: "$_id",
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: {
+            $mergeObjects: [{ requestId: "$requestId" }, "$user_details"],
+          },
+        },
+      },
+      {
+        $project: {
+          refreshToken: 0,
+          password: 0,
+          __v: 0,
+        },
+      }
+    );
+
+    const request = await Request.aggregate(pipeline);
+
+    res.status(200).json(new ApiResponse(200, request));
+  }
+);
 
 export const sendFriendRequest = asyncHandler(
   async (req: RequestExpress, res: Response) => {
--- apps/backend/src/routes/request.routes.ts
+++ apps/backend/src/routes/request.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { sendFriendRequest } from "../controllers/request.controller.js";
+import {
+  getRequests,
+  sendFriendRequest,
+} from "../controllers/request.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/").get(verifyJWT);
+router.route("/").get(verifyJWT, getRequests);
 
 router.route("/").post(verifyJWT, sendFriendRequest);
 
