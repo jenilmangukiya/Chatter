99f2402bb9eb7e61a399b4cbc41e7e056685232c
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-06-02 14:18:53 +0530

    tryy 5

--- apps/backend/src/App.ts
+++ apps/backend/src/App.ts
@@ -1,33 +1,18 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import express from "express";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-); // Cors Middleware
-
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
-
-// Routes
-import healthCheckRouter from "./routes/healthCheck.routes.js";
-import requestRouter from "./routes/request.routes.js";
-import userRouter from "./routes/user.routes.js";
-
-// Routes Declaration
-app.use("/", (req, res) => {
-  res.status(200).json({ status: "success", message: "success" });
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
-app.use("/api/v1/healthCheck", healthCheckRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/requests", requestRouter);
+
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 export { app };
 export default app;
