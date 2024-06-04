6f40a6671674d9c65ff036e554dca7c0e82321c7
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-06-04 21:40:24 +0530

    [BE] feat: add get chat API

--- apps/backend/src/controllers/chat.controller.ts
+++ apps/backend/src/controllers/chat.controller.ts
@@ -1,6 +1,8 @@
 import { Response } from "express";
-import { PipelineStage } from "mongoose";
+import { ObjectId } from "mongodb";
+import mongoose, { PipelineStage } from "mongoose";
 import { Chat } from "../models/chat.model.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { RequestExpress } from "../utils/types.js";
@@ -50,5 +52,61 @@ export const getUserChatsList = asyncHandler(
     const request = await Chat.aggregate(pipeline);
 
     res.status(200).json(new ApiResponse(200, request));
+  }
+);
+
+export const getChat = asyncHandler(
+  async (req: RequestExpress, res: Response) => {
+    const { chatId } = req.params;
+    console.log("chatId", chatId);
+
+    if (!mongoose.Types.ObjectId.isValid(chatId))
+      throw new ApiError(404, "Invalid chatId");
+
+    const chat = await Chat.aggregate([
+      {
+        $match: {
+          _id: new ObjectId(chatId),
+        },
+      },
+      {
+        $lookup: {
+          from: "chatmembers",
+          localField: "_id",
+          foreignField: "chat",
+          as: "users",
+          pipeline: [
+            {
+              $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user_data",
+              },
+            },
+            {
+              $unwind: "$user_data",
+            },
+            {
+              $replaceRoot: {
+                newRoot: "$user_data",
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                email: 1,
+                fullName: 1,
+                avatar: 1,
+              },
+            },
+          ],
+        },
+      },
+    ]);
+
+    if (!chat) throw new ApiError(404, "Chat not found");
+
+    res.status(200).json(new ApiResponse(200, chat));
   }
 );
--- apps/backend/src/routes/chat.routes.ts
+++ apps/backend/src/routes/chat.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getUserChatsList } from "../controllers/chat.controller.js";
+import { getChat, getUserChatsList } from "../controllers/chat.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/").get(verifyJWT, getUserChatsList);
+
+router.route("/:chatId").get(verifyJWT, getChat);
 
 export default router;
