93280d1ee23a8ce6b9b78068ecef2cdfc387c2da
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-06-13 21:16:28 +0530

    [FE] cookie not accessed after login

--- apps/frontend/src/Auth/axiosAuth.ts
+++ apps/frontend/src/Auth/axiosAuth.ts
@@ -4,16 +4,15 @@ import { getCookie } from "../utils";
 const HEADERS: any = {
   "Content-Type": "application/json",
   Accept: "application/json",
-  Authorization: getCookie("accessToken") || "",
 };
 
 const instance = axios.create({
   withCredentials: true,
 });
 
-instance.interceptors.request.use((config) => {
+instance.interceptors.request.use((config: any) => {
   config.baseURL = import.meta.env.VITE_BASE_URL || "http://localhost:3001";
-
+  config.Authorization = getCookie("accessToken") || "";
   config.headers = {
     ...HEADERS,
     ...config.headers,
